bf76cea56a1d9ff620cccb4f07e4d13eb75c6e3a
Mitama <loligothick@gmail.com>
2019-09-29 17:51:04 +0900

    extract inline module

---
+++ src/bot.rs
@@ -0,0 +1,143 @@
+use super::dictionary::*;
+use indexmap::IndexSet;
+use rand::distributions::{Distribution, Uniform};
+use std::collections::{BTreeMap, BTreeSet};
+use std::sync::RwLock;
+use std::sync::{Arc, Mutex};
+
+custom_derive! {
+    #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, NextVariant, PrevVariant)]
+    pub enum Lang {
+        En,
+        Ja,
+        Fr,
+        De,
+        It,
+    }
+}
+
+impl Lang {
+    pub fn as_symbol(&self) -> String {
+        match self {
+            Lang::En => "英単語".to_string(),
+            Lang::Ja => "単語".to_string(),
+            Lang::Fr => "仏単語".to_string(),
+            Lang::De => "独単語".to_string(),
+            Lang::It => "伊単語".to_string(),
+        }
+    }
+}
+
+impl<S: Into<String>> From<S> for Lang {
+    fn from(s: S) -> Self {
+        let lang: String = s.into();
+        match lang {
+            en if en == "en" => Lang::En,
+            ja if ja == "ja" => Lang::Ja,
+            fr if fr == "fr" => Lang::Fr,
+            de if de == "de" => Lang::De,
+            it if it == "it" => Lang::It,
+            _ => panic!("unexpected language token!"),
+        }
+    }
+}
+
+pub enum Status {
+    StandingBy,
+    Holding(String, String, BTreeSet<String>, Option<BTreeSet<String>>),
+    Contesting(
+        String,
+        String,
+        (u32, u32),
+        BTreeSet<String>,
+        Option<BTreeSet<String>>,
+    ),
+}
+
+impl Status {
+    pub fn is_standing_by(&self) -> bool {
+        match self {
+            Status::StandingBy => true,
+            _ => false,
+        }
+    }
+    pub fn is_holding(&self) -> bool {
+        match self {
+            Status::Holding(..) => true,
+            _ => false,
+        }
+    }
+    pub fn is_contesting(&self) -> bool {
+        match self {
+            Status::Contesting(..) => true,
+            _ => false,
+        }
+    }
+
+    pub fn ans(&self) -> std::result::Result<&String, ()> {
+        match self {
+            Status::StandingBy => Err(()),
+            Status::Holding(ans, ..) | Status::Contesting(ans, ..) => Ok(ans),
+        }
+    }
+}
+
+pub fn select_dictionary(lang: Lang) -> &'static Dictionary {
+    match lang {
+        Lang::En => &*ENGLISH,
+        Lang::Ja => &*JAPANESE,
+        Lang::Fr => &*FRENCH,
+        Lang::De => &*GERMAN,
+        Lang::It => &*ITALIAN,
+    }
+}
+
+pub fn select_dictionary_from_str<S: Into<String>>(lang: S) -> &'static Dictionary {
+    let lang_string: String = lang.into();
+    match lang_string {
+        en if en == "en" => &*ENGLISH,
+        ja if ja == "ja" => &*JAPANESE,
+        fr if fr == "fr" => &*FRENCH,
+        de if de == "de" => &*GERMAN,
+        it if it == "it" => &*ITALIAN,
+        _ => panic!("unexpected language token!"),
+    }
+}
+
+pub struct DictionarySelector {
+    engine: Result<Lang, Uniform<usize>>,
+    set: IndexSet<Lang>,
+}
+
+impl DictionarySelector {
+    pub fn new() -> DictionarySelector {
+        DictionarySelector {
+            engine: Ok(Lang::En),
+            set: Default::default(),
+        }
+    }
+    pub fn set<S: Into<String>>(&mut self, languages: Vec<S>) {
+        if languages.len() == 1 {
+            self.engine = Ok(Lang::from(languages.into_iter().next().unwrap()));
+        } else {
+            self.engine = Err(Uniform::new(0, languages.len()));
+            for lang in languages {
+                self.set.insert(Lang::from(lang));
+            }
+        }
+    }
+    pub fn select<Engine: rand::Rng>(&self, rng: &mut Engine) -> (&'static Dictionary, Lang) {
+        let lang = *self
+            .engine
+            .as_ref()
+            .unwrap_or_else(|uniform| self.set.get_index(uniform.sample(rng)).unwrap());
+        (select_dictionary(lang.clone()), lang)
+    }
+}
+
+lazy_static! {
+    pub static ref QUIZ: Arc<Mutex<Status>> = Arc::new(Mutex::new(Status::StandingBy));
+    pub static ref CONTEST_REUSLT: RwLock<BTreeMap<String, u32>> = RwLock::new(BTreeMap::new());
+    pub static ref CONTEST_LIBRARY: RwLock<DictionarySelector> =
+        RwLock::new(DictionarySelector::new());
+}
---
+++ src/command.rs
@@ -0,0 +1,79 @@
+use clap::{App, AppSettings, Arg};
+
+fn range_validator(low: u32, up: u32) -> Box<dyn Fn(String) -> Result<(), String>> {
+    Box::new(move |num: String| match num.parse::<u32>() {
+        Err(_) => Err(String::from(
+            "please specify unsigned integer after '~contest'.",
+        )),
+        Ok(num) if num == low => Err(String::from("too small number.")),
+        Ok(num) if num > up => Err(String::from("too large number.")),
+        Ok(_) => Ok(()),
+    })
+}
+
+fn parse_validator<T: std::str::FromStr>(num: String) -> Result<(), String> {
+    num.parse::<T>()
+        .map(|_| ())
+        .map_err(|_| format!("`{}` is invalid.", num).to_string())
+}
+
+fn language_validator(language: String) -> Result<(), String> {
+    if !crate::QUIZ_COMMANDS_REGEX.is_match(&language) {
+        Err(format!("unexpected language '{}'.", language).to_string())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn contest(
+    args: &mut serenity::framework::standard::Args,
+) -> clap::Result<(u32, Vec<String>)> {
+    App::new("contest")
+        .version(crate::VERSION)
+        .setting(AppSettings::ColorNever)
+        .arg(
+            Arg::with_name("number")
+                .required(true)
+                .validator(range_validator(1, 100)),
+        )
+        .arg(
+            Arg::with_name("languages")
+                .required(true)
+                .use_delimiter(true)
+                .validator(language_validator)
+                .min_values(1),
+        )
+        .get_matches_from_safe(
+            vec!["contest".to_string()]
+                .into_iter()
+                .chain(args.iter::<String>().filter_map(Result::ok))
+                .into_iter(),
+        )
+        .map(|a| {
+            let num = a.value_of("number").unwrap().parse::<u32>().unwrap();
+            let languages = a
+                .values_of("languages")
+                .unwrap()
+                .map(str::to_string)
+                .collect::<Vec<_>>();
+            (num, languages)
+        })
+}
+
+pub fn hint(args: &mut serenity::framework::standard::Args) -> clap::Result<usize> {
+    App::new("hint")
+        .version(crate::VERSION)
+        .setting(AppSettings::ColorNever)
+        .arg(
+            Arg::with_name("number")
+                .required(true)
+                .validator(parse_validator::<usize>),
+        )
+        .get_matches_from_safe(
+            vec!["contest".to_string()]
+                .into_iter()
+                .chain(args.iter::<String>().filter_map(Result::ok))
+                .into_iter(),
+        )
+        .map(|a| a.value_of("number").unwrap().parse::<usize>().unwrap())
+}
